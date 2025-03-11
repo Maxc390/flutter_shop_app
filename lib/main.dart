8d7e3e1c3a3d4416dc1433e080157ed84cac3621
Maxc390 <maxwell.chanzu@students.jkuat.ac.ke>
2025-03-11 07:34:37 +0300

    added Cart page and state management using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_flutter/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/cart_provider.dart';
+import 'package:shop_app_flutter/pages/home_page.dart';
 // import 'package:shop_app_flutter/product_details_page.dart';
 // import './global_variables.dart';
 
@@ -12,31 +14,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
-      theme: ThemeData(
-          inputDecorationTheme: const InputDecorationTheme(
-              hintStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          )),
-          fontFamily: 'Lato',
-          useMaterial3: true,
-          appBarTheme: const AppBarTheme(
-              centerTitle: true,
-              titleTextStyle: TextStyle(fontSize: 20, color: Colors.black)),
-          textTheme: const TextTheme(
-              titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-              titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              bodySmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromRGBO(254, 206, 1, 1),
-              primary: const Color.fromRGBO(254, 206, 1, 1))),
-      // home: ProductDetailsPage(
-      //   product: products[0],
-      // ),
-      home: const HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping App',
+        theme: ThemeData(
+            inputDecorationTheme: const InputDecorationTheme(
+                hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            )),
+            fontFamily: 'Lato',
+            useMaterial3: true,
+            appBarTheme: const AppBarTheme(
+                centerTitle: true,
+                titleTextStyle: TextStyle(fontSize: 20, color: Colors.black)),
+            textTheme: const TextTheme(
+                titleLarge:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+                titleMedium:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                bodySmall:
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color.fromRGBO(254, 206, 1, 1),
+                primary: const Color.fromRGBO(254, 206, 1, 1))),
+        // home: ProductDetailsPage(
+        //   product: products[0],
+        // ),
+        home: const HomePage(),
+      ),
     );
   }
 }
--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
@@ -10,6 +12,25 @@ class ProductDetailsPage extends StatefulWidget {
 
 class _ProductDetailsPageState extends State<ProductDetailsPage> {
   int selectedSize = 0;
+  void onTap() {
+    if (selectedSize != 0) {
+      Provider.of<CartProvider>(context, listen: false).addProduct({
+        'id': widget.product['id'],
+        'title': widget.product['title'],
+        'price': widget.product['price'],
+        'imageUrl': widget.product['imageUrl'],
+        'company': widget.product['company'],
+        'size': selectedSize,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Product Added Successfully!!'),
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Please Select A Size'),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +95,7 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: onTap,
                     style: ElevatedButton.styleFrom(
                         backgroundColor: Theme.of(context).colorScheme.primary,
                         minimumSize: const Size(double.infinity, 50)),
